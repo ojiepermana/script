477cc48568f2f48f4e82dbdf143645d30840cdf5
ojiepermana <me@ojiepermana.com>
2024-11-16 08:59:00 +0700

    Create script.sh

---
+++ centos-9/firewall/script.sh
@@ -0,0 +1,10 @@
+sudo dnf install firewalld -y
+sudo systemctl enable firewalld
+sudo systemctl start firewalld
+sudo firewall-cmd --permanent --add-service=http
+sudo firewall-cmd --permanent --add-service=https
+sudo firewall-cmd --permanent --list-all
+sudo firewall-cmd --permanent --zone=public --add-port=3000-3999/tcp
+sudo firewall-cmd --permanent --zone=public --add-port=81-89/tcp
+
+sudo firewall-cmd --reload
