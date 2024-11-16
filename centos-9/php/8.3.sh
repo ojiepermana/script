cd2fc92eb643477802d0135d298eefde74e4c283
ojiepermana <me@ojiepermana.com>
2024-11-16 10:41:09 +0700

    add puppeter

--- centos-9/php/8.3.sh
+++ centos-9/php/8.3.sh
@@ -3,7 +3,8 @@ sudo dnf install remi-release -y
 sudo dnf install https://rpms.remirepo.net/enterprise/remi-release-9.rpm -y
 sudo dnf module list php
 sudo dnf module enable php:remi-8.3 -y
-sudo dnf install php -y
+sudo dnf install php php-fpm php-cli php-mbstring php-xml php-zip php-curl php-json php-gd php-opcache php-intl php-bcmath -y
+
 
 wget https://getcomposer.org/installer -O composer-installer.php
 sudo php composer-installer.php --filename=composer --install-dir=/usr/local/bin
---
+++ centos-9/puppeter/install.sh
@@ -0,0 +1,15 @@
+sudo npm install -g puppeteer
+
+# Create a repo file for Google Chrome
+sudo tee /etc/yum.repos.d/google-chrome.repo <<EOF
+[google-chrome]
+name=google-chrome
+baseurl=https://dl.google.com/linux/chrome/rpm/stable/x86_64
+enabled=1
+gpgcheck=1
+gpgkey=https://dl.google.com/linux/linux_signing_key.pub
+EOF
+
+# Install Google Chrome
+sudo dnf install -y google-chrome-stable
+sudo chmod -R o+rx /usr/lib/node_modules/puppeteer/.local-chromium
