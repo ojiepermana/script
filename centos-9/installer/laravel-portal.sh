db103c341adf6d354a76c7e1276871c249aba4b7
ojiepermana <me@ojiepermana.com>
2025-04-08 13:11:35 +0700

    ups

--- centos-9/installer/laravel-portal.sh
+++ centos-9/installer/laravel-portal.sh
@@ -1,7 +1,9 @@
 sudo dnf update -y
-sudo dnf install wget nano curl htop -y
+sudo dnf install wget nano curl htop git zip unzip -y
 
 curl -s "https://raw.githubusercontent.com/ojiepermana/script/refs/heads/main/centos-9/php/8.3.sh" | sh
 curl -s "https://raw.githubusercontent.com/ojiepermana/script/refs/heads/main/centos-9/firewall/script.sh" | sh
 curl -s "https://raw.githubusercontent.com/ojiepermana/script/refs/heads/main/centos-9/nginx/nginx.sh" | sh
 curl -s "https://raw.githubusercontent.com/ojiepermana/script/refs/heads/main/centos-9/nodejs/22.sh" | sh
+
+
