b0f77ae709a9bec9e61199b850d40c1c6a2b59ea
ojiepermana <me@ojiepermana.com>
2025-04-07 21:57:10 -0500

    postgres

---
+++ centos-9/postgresql/17.sh
@@ -0,0 +1,7 @@
+dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+dnf -qy module disable postgresql
+dnf install -y postgresql17-server
+/usr/pgsql-17/bin/postgresql-17-setup initdb
+systemctl enable postgresql-17
+systemctl start postgresql-17
+systemctl restart postgresql-17
